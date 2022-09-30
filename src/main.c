9fd352b896fd73c5058c2e9860890ae3af635db2
ikarjala <ikarjala@student.hive.fi>
2022-09-30 22:54:29 +0300

    parser wip, hardcoded map, file name is appended to win_title

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 20:55:03 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/09/21 22:44:38 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/09/28 19:33:58 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,21 @@ static inline int	print_usage(void)
 
 int	main(int argc, char **argv)
 {
-	//char	wname_buf[10];
+	char	*wname;
 	t_vars	v;
 
 	if (argc != 2)
 		return (print_usage());
 	v.uptime = 0;
-	v.fdf.map = parse_map_file(argv[1]);
+	v.fdf = parse_map_file(argv[1]);
 	v.sig = SIG_CONT;
 
 	v.mlxo = mlx_init();
-	v.mlx_win = mlx_new_window(v.mlxo, WIN_RESX, WIN_RESY, WIN_TITLE);
+
+	wname = ft_strjoin (WIN_TITLE " : ", argv[1]);
+	v.mlx_win = mlx_new_window(v.mlxo, WIN_RESX, WIN_RESY, wname);
+	ft_strdel (&wname);
+
 	v.img.o = mlx_new_image(v.mlxo, WIN_RESX, WIN_RESY);
 	v.img.addr = mlx_get_data_addr(v.img.o, &v.img.bppx,
 			&v.img.width, &v.img.endian);
--- src/parser.c
+++ src/parser.c
@@ -6,40 +6,114 @@
 /*   By: ikarjala <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 14:41:12 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/09/23 16:12:56 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/09/30 22:40:02 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static inline int	parse_index(char *s)
-{
-	int	val;
+#define BUFF_SIZE	128
 
-	val = 0;
-	s = NULL;
-	/*
-	while (ft_isdigit(*s))
-		val = val * 10 + (*s - '0');
-		*/
-	return (val);
+t_fdf	initialize_fdf_data(void)
+{
+	t_fdf	fdf;
+
+	fdf.map = NULL;
+	fdf.w = 0;
+	fdf.h = 0;
+	fdf.amplitude = 5;
+	fdf.scale = 50;
+	return (fdf);
 }
 
-int	parse_map_file(char *fname)
+#if 0
+/* Find out how many rows there are and check
+ * that the amount of columns is consistent,
+ * then return the fd or -1 in case of error
+*/
+static inline int	open_validate(char *fname, t_fdf *fdf)
 {
-	//const size_t	buff_size = 128;
-	//char	buf[buff_size];
-	//ssize_t	rb;
+	char	buf[BUFF_SIZE];
+	ssize_t	rb;
 	int		fd;
+	char	*cp;
 	
-	//fd = open(fname, RD_RDONLY);
-	fd = parse_index(fname);
-	fname = NULL;
-	return (0);
-	/*
-	while (rb = read(fd, buf, buff_size))
+	fd = open(fname, RD_RDONLY);
+	rb = read(fd, buf, BUFF_SIZE);
+	if (rb > 0)
 	{
-		
+		cp = ft_memchr(buf, '\n', BUFF_SIZE)
+		while (cp)
+		{
+			cp = ft_memchr(cp + 1, '\n', sizeof(buf) - (cp - buf));
+			fdf->h += (cp != NULL);
+		}
 	}
-	*/
+	close (fd);
+	fd = open(fname, RD_RDONLY);
+	return (fd);
 }
+
+t_fdf	parse_map_file(char *fname)
+{
+	t_fdf	fdf;
+	t_list	*wordl;
+	int		fd;
+	int		x;
+	int		y;
+	
+	fdf = initialize_fdf_data();
+	fd = open_validate(fname, &fdf);
+	if (fd < 0)
+		return (-1);
+	fdf.map = (int **)malloc(sizeof(int *) * fdf.h);
+	// read until EOF
+	// strsplit, count words
+	// foreach word, atoi
+	y = 0;
+	while (y < fdf.h)
+	{
+		wordl = ft_memsplit(buf, " ", "\n", BUFF_SIZE);
+		x = 0;
+		while (x < fdf.w)
+		{
+			fdf.map[x][y] = ft_atoi(wordl->content);
+			x ++;
+		}
+		if (fdf->w != ft_lstlen(wordl));
+			// return -1
+		ft_lstdel (wordl);
+		y ++;
+	}
+	return (fdf);
+}
+
+#else
+t_fdf	parse_map_file(char *fname)
+{
+	const int	w = 10;
+	const int	h = 7;
+	const int	map[h][w] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+							 {0,10, 0,10, 0, 0,10,10,10, 0},
+							 {0,10, 0,10, 0, 0, 0, 0,10, 0},
+							 {0,10,10,10, 0, 0,10,10,10, 0},
+							 {0, 0, 0,10, 0, 0,10, 0, 0, 0},
+							 {0, 0, 0,10, 0, 0,10,10,10, 0},
+							 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+	t_fdf		fdf;
+
+	fname = NULL;
+	fdf = initialize_fdf_data();
+	fdf.w = w;
+	fdf.h = h;
+	fdf.map = (int **)malloc(sizeof(int *) * w);
+	for (int x = 0; x < w; x++)
+	{
+		fdf.map[x] = (int *)malloc(sizeof(int) * h);
+		for (int y = 0; y < h; y++)
+			fdf.map[x][y] = map[y][x];
+	}
+	return (fdf);
+}
+
+#endif
